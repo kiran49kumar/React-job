5b37a97422e865d5abf46a243c1ca534d9195088
kiran <kirankumarbhatra49@gmail.com>
2024-08-15 00:18:23 +0530

    Limit Jobs to 3 and Created ViewAllJobs components

--- react-job/src/App.jsx
+++ react-job/src/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListing from './components/JobListings'
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import HomeCards from './components/HomeCards';
+import JobListing from './components/JobListings';
+import ViewAllJobs from './components/ViewAllJobs';
 
 const App = () => {
   return (
@@ -11,19 +12,8 @@ const App = () => {
     <Hero />
     <HomeCards />
     <JobListing />
-
+    <ViewAllJobs />
     
-
-    {/* <!-- Browse Jobs --> */}
-    
-
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
     </>
   )
 }
