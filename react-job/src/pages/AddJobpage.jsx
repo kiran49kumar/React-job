c84637eaf7e9fbbb78fb197cab895da96dca0c1f
kiran <kirankumarbhatra49@gmail.com>
2024-08-16 00:18:08 +0530

    erro correction

--- react-job/src/pages/AddJobpage.jsx
+++ react-job/src/pages/AddJobpage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { SiAlacritty } from "react-icons/si";
+
 
 const AddJobpage = () => {
     const [title, setTitle] = useState('');
