a75e006338d41beef53e49432bc2b0a4acb51357
kiran <kirankumarbhatra49@gmail.com>
2024-08-15 23:35:19 +0530

    Add job page

--- react-job/src/App.jsx
+++ react-job/src/App.jsx
@@ -10,12 +10,14 @@ import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, {jobLoader} from './pages/Jobpage';
+import AddJobpage from './pages/AddJobpage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
     <Route index element= {<HomePage />} />
     <Route path='/jobs' element= {<JobsPage />} />
+    <Route path='/add-job' element= {<AddJobpage />} />
     <Route path='/jobs/:id' element= {<JobPage />} loader={jobLoader} />
     <Route path='*' element= {<NotFoundPage />} />
   </Route>
