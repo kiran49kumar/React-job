2d2b4a3e05878258060f90fb067643ff4968e281
kiran <kirankumarbhatra49@gmail.com>
2024-08-15 16:50:12 +0530

    Link components

--- react-job/src/components/HomeCards.jsx
+++ react-job/src/components/HomeCards.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import { Link } from "react-router-dom";
+import Card from "./Card";
 
 const HomeCards = () => {
   return (
@@ -7,33 +8,33 @@ const HomeCards = () => {
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
           <Card>
-          <h2 className="text-2xl font-bold">For Developers</h2>
+            <h2 className="text-2xl font-bold">For Developers</h2>
             <p className="mt-2 mb-4">
               Browse our React jobs and start your career today
             </p>
-            <a
-              href="/jobs.html"
+            <Link
+              to="/jobs"
               className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
             >
               Browse Jobs
-            </a>
+            </Link>
           </Card>
-          <Card bg='bg-indigo-100'>
-          <h2 className="text-2xl font-bold">For Employers</h2>
+          <Card bg="bg-indigo-100">
+            <h2 className="text-2xl font-bold">For Employers</h2>
             <p className="mt-2 mb-4">
               List your job to find the perfect developer for the role
             </p>
-            <a
-              href="/add-job.html"
+            <Link
+              to="/add-job"
               className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
             >
               Add Job
-            </a>
+            </Link>
           </Card>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default HomeCards
+export default HomeCards;
