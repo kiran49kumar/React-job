89f2f474d55f8b8765cbece0b53737651659fe59
kiran <kirankumarbhatra49@gmail.com>
2024-08-15 16:06:10 +0530

    React Router setup, Create Routes From Elements,Router Provider

--- react-job/src/App.jsx
+++ react-job/src/App.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
+import { 
+  Route,
+  createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import HomeCards from './components/HomeCards';
 import JobListing from './components/JobListings';
 import ViewAllJobs from './components/ViewAllJobs';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element= {<h1>My App</h1>} />)
+);
+
 const App = () => {
-  return (
-    <>
-    <Navbar /> 
-    <Hero />
-    <HomeCards />
-    <JobListing />
-    <ViewAllJobs />
-    
-    </>
-  )
-}
+  return <RouterProvider router={router}/>
+};
 
 export default App
