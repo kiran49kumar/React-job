69a82fe17138a7260de09af2489dc36386fe63ae
kiran <kirankumarbhatra49@gmail.com>
2024-08-15 16:28:00 +0530

    Created MainLayout Stored HomePage in it. Layout and Outlet

--- react-job/src/layouts/MainLayout.jsx
+++ react-job/src/layouts/MainLayout.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
-import HomeCards from '../components/HomeCards';
-import JobListings from '../components/JobListings';
-import ViewAllJobs from '../components/ViewAllJobs'
 
 const MainLayout = () => {
   return (
     <>
         <Navbar />
         <Outlet />
-        <HomeCards />
-        <JobListings />
-        <ViewAllJobs />
     </>
   )
 }
