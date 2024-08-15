9fa95cb33501510da2502df7fdc3357432a5b0d1
kiran <kirankumarbhatra49@gmail.com>
2024-08-15 16:11:55 +0530

    Created HomePage Component

--- react-job/src/App.jsx
+++ react-job/src/App.jsx
@@ -5,14 +5,10 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListing from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
+import HomePage from './pages/HomePage';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element= {<h1>My App</h1>} />)
+  createRoutesFromElements(<Route index element= {<HomePage />} />)
 );
 
 const App = () => {
