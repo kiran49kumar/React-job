8eab952951838d923d4b81d1a102fb56fecfc43e
kiran <kirankumarbhatra49@gmail.com>
2024-08-17 12:45:40 +0530

    pass function as Prop

--- react-job/src/App.jsx
+++ react-job/src/App.jsx
@@ -12,19 +12,24 @@ import NotFoundPage from './pages/NotFoundPage';
 import JobPage, {jobLoader} from './pages/Jobpage';
 import AddJobpage from './pages/AddJobpage';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-  <Route path='/' element={<MainLayout />}>
-    <Route index element= {<HomePage />} />
-    <Route path='/jobs' element= {<JobsPage />} />
-    <Route path='/add-job' element= {<AddJobpage />} />
-    <Route path='/jobs/:id' element= {<JobPage />} loader={jobLoader} />
-    <Route path='*' element= {<NotFoundPage />} />
-  </Route>
-  )
-);
 
 const App = () => {
+  const addJob = (newJob) =>{
+    console.log(newJob);
+  }
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element= {<HomePage />} />
+      <Route path='/jobs' element= {<JobsPage />} />
+      <Route path='/add-job' element= {<AddJobpage addJobSubmit={addJob} />} />
+      <Route path='/jobs/:id' element= {<JobPage />} loader={jobLoader} />
+      <Route path='*' element= {<NotFoundPage />} />
+    </Route>
+    )
+  );
+
   return <RouterProvider router={router}/>
 };
 
