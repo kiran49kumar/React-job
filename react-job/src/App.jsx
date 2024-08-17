62f5ce96bf58a31894626db2dab67722907051cb
kiran <kirankumarbhatra49@gmail.com>
2024-08-17 19:03:14 +0530

    POST request to add jobs

--- react-job/src/App.jsx
+++ react-job/src/App.jsx
@@ -14,8 +14,15 @@ import AddJobpage from './pages/AddJobpage';
 
 
 const App = () => {
-  const addJob = (newJob) =>{
-    console.log(newJob);
+  const addJob = async (newJob) =>{
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
   }
 
   const router = createBrowserRouter(
